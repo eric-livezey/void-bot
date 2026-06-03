75c13b84a0123085a068040c20eaf2ab84ff9ac2
eric-livezey <eric.livezey@gmail.com>
2026-06-03 00:03:57 -0400

    More robust video ID extraction from music search results

--- commands/playback/playmusic.ts
+++ commands/playback/playmusic.ts
@@ -1,9 +1,23 @@
 import { InteractionContextType, PermissionsBitField, SlashCommandBuilder, SlashCommandStringOption } from 'discord.js';
+import { YTNodes } from 'youtubei.js';
 import { Command } from '..';
 import { CommandContext, InteractionContext, MessageContext } from '../../context';
 import { getInnertubeInstance } from '../../innertube';
 import { Track } from '../../player';
 import { connectToSpeak, playTrack } from './play';
+
+function getVideoIdFromSearchResult(result: YTNodes.MusicResponsiveListItem) {
+    if (result.id != null) {
+        return result.id;
+    }
+    return [
+        result.endpoint,
+        result.overlay?.content?.endpoint,
+        ...result.menu?.contents
+            .filterType(YTNodes.MenuNavigationItem)
+            .flatMap(n => [n.endpoint, n.endpoint.command]) ?? []
+    ].find(e => e?.is(YTNodes.WatchEndpoint))?.as(YTNodes.WatchEndpoint).buildRequest().videoId;
+}
 
 export async function playMusic(ctx: CommandContext<true>, query: string) {
     if (!await connectToSpeak(ctx)) {
@@ -16,7 +30,12 @@ export async function playMusic(ctx: CommandContext<true>, query: string) {
         await ctx.reply('There were no valid results for your query.');
         return;
     }
-    const track = await Track.fromVideoId(items.songs.contents[0].id!);
+    const videoId = getVideoIdFromSearchResult(items.songs.contents[0]);
+    if (videoId == null) {
+        await ctx.reply('Failed to extract video ID from search result.');
+        return;
+    }
+    const track = await Track.fromVideoId(videoId);
 
     await playTrack(ctx, track);
 }
